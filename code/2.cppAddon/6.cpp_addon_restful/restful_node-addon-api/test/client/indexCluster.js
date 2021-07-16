9bf3ab381960e19583a3b10acb268c04691c1f33
itas109 <itas109@qq.com>
2021-07-16 22:12:45 +0800

    optimize Concurrent
    Signed-off-by: itas109 <itas109@qq.com>

--- code/2.cppAddon/6.cpp_addon_restful/restful_node-addon-api/test/client/indexCluster.js
+++ code/2.cppAddon/6.cpp_addon_restful/restful_node-addon-api/test/client/indexCluster.js
@@ -7,12 +7,12 @@ let args = process.argv.slice(2);
 console.log('args: ' + args);
 
 const cluster = require('cluster');
-let CPUNums = require('os').cpus().length;
+let CPU_NUM = require('os').cpus().length;
 
 if(args[0] !== undefined && args[0] > 0){
-    CPUNums = args[0];
+    CPU_NUM = args[0];
 }else{
-    CPUNums *= 2; // CPU核数的双倍
+    CPU_NUM *= 2; // CPU核数的双倍
 }
 // CPUNums = 8;
 
@@ -22,24 +22,38 @@ const url_ip = '127.0.0.1';
 const url_port = 8080;
 const url_path = 'test';
 
-var count = 1;
+let EXECUTE_PERIOD_MS = 1000; // 1秒
+if (args[1] !== undefined && args[1] > 0) {
+    EXECUTE_PERIOD_MS = args[1];
+} else {
+    EXECUTE_PERIOD_MS = 1000; // 1秒
+}
 
 if (cluster.isMaster) {
-    console.log(`主进程 ${process.pid} 正在运行  集群个数: ${CPUNums}`);
+    console.log(`主进程 ${process.pid} 正在运行  集群个数: ${CPU_NUM}`);
 
     // 衍生进程
-    for (let i = 0; i < CPUNums; i++) {
-        cluster.fork();
+    let workers = [];
+    for (let i = 0; i < CPU_NUM; i++) {
+        workers.push(cluster.fork());
     }
+
+    setInterval(() => {
+        workers.forEach(worker => {
+            worker.send('run');
+        });
+    }, EXECUTE_PERIOD_MS);
 
     cluster.on('exit', (work, code, signal) => {
         console.log(`工作进程 ${work.process.pid} 已退出`);
     })
 } else {
 
-    console.log(`工作进程 ${count} ${process.pid} 正在运行  `);
+    console.log(`工作进程 ${process.pid} 正在运行`);
 
-    count++;
+    process.on('message', (msg) => {
+        runRequestRandom();
+    });
 
     setInterval(runRequestRandom, 1000);
 }
@@ -50,6 +64,11 @@ function runRequestRandom() {
 
     (async () => {
         const response  = await got.get(url, {responseType: 'json'});
-        console.log((new Date().getTime() - startTime) + 'ms, '+ JSON.stringify(response.body));
+        let result = JSON.stringify(response.body);
+            if (result.length < 100) {
+                console.log((new Date().getTime() - startTime), 'ms, ', result);
+            } else {
+                console.log((new Date().getTime() - startTime), 'ms, resutl length ', result.length);
+            }
     })();
 }
